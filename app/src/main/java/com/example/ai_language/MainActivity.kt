e4ca503a0825db2141a8377909d77943684c4fff
Seongmin <arcn1345@gmail.com>
2024-01-08 17:10:23 +0900

    aa1331131

--- app/src/main/java/com/example/ai_language/MainActivity.kt
+++ app/src/main/java/com/example/ai_language/MainActivity.kt
@@ -9,6 +9,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        //22qwaa
+        //22qwaaaa
     }
 }
