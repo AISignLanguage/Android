cf2a28fe79a080ad4133a2da1311e771f0cbd0b8
shjin <shjin4667@gmail.com>
2024-01-08 17:04:22 +0900

    dddd

--- app/src/main/java/com/example/ai_language/MainActivity.kt
+++ app/src/main/java/com/example/ai_language/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 //test
+//dldldld
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
