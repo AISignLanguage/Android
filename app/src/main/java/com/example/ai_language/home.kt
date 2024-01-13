f373da810050bb6b4c7774e59349078dc319e3b1
jiwon <pjw99032986@gmail.com>
2024-01-13 11:29:12 +0900

    dic recycler

--- app/src/main/java/com/example/ai_language/home.kt
+++ app/src/main/java/com/example/ai_language/home.kt
@@ -1,11 +1,20 @@
 package com.example.ai_language
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
 
 class home : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+
+        val dic_btn = findViewById<ImageButton>(R.id.dic_btn)
+        dic_btn.setOnClickListener {
+            val intent = Intent(this,DictionaryPage::class.java)
+            startActivity(intent)
+        }
     }
 }
