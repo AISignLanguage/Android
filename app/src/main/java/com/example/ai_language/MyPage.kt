c07a83d4a7f0c6609e5b08db01a2f43eff1fee22
jiwon <pjw99032986@gmail.com>
2024-01-13 22:24:29 +0900

    연결

--- app/src/main/java/com/example/ai_language/MyPage.kt
+++ app/src/main/java/com/example/ai_language/MyPage.kt
@@ -1,11 +1,33 @@
 package com.example.ai_language
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MyPage : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_my_page)
+
+
+        val my_inform_edit = findViewById<TextView>(R.id.my_inform_edit)
+        my_inform_edit.setOnClickListener {
+            val intent = Intent(this,PersonalInfo::class.java)
+            startActivity(intent)
+        }
+
+
+        val withDrawer = findViewById<TextView>(R.id.withdrawal)
+        withDrawer.setOnClickListener {
+            val intent = Intent(this,Unregister::class.java)
+            startActivity(intent)
+        }
+
+        val sign_out = findViewById<TextView>(R.id.sign_out)
+        sign_out.setOnClickListener {
+
+        }
+
     }
 }
---
+++ app/src/main/java/com/example/ai_language/Personalinfo.kt
@@ -0,0 +1,10 @@
+package com.example.ai_language
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class PersonalInfo : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_personal_info)
+    }
+}
