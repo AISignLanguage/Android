7409e08ae688310df5015727c24482c8937c288f
jiwon <pjw99032986@gmail.com>
2024-01-13 11:17:31 +0900

    dic recycler

--- app/src/main/java/com/example/ai_language/MainActivity.kt
+++ app/src/main/java/com/example/ai_language/MainActivity.kt
@@ -1,7 +1,10 @@
 ﻿package com.example.ai_language
 
+import com.example.ai_language.DictionaryPage
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import androidx.viewpager2.widget.ViewPager2
 import me.relex.circleindicator.CircleIndicator3
 
@@ -10,6 +13,10 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
+        val bt = findViewById<Button>(R.id.button)
+        bt.setOnClickListener{
+            val intent = Intent(this, DictionaryPage::class.java)
+            startActivity(intent)
+        }
     }
 }
