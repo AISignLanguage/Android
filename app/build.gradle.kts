8c10b4e22a4ed52d80dfd9f4ea7604ecfd38bbbc
shjin <shjin4667@gmail.com>
2024-01-15 19:24:56 +0900

    뉴스 리사이클러뷰

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,9 +43,11 @@ dependencies {
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation ("androidx.viewpager2:viewpager2:1.0.0")
     implementation ("me.relex:circleindicator:2.1.6")
+    implementation ("com.github.bumptech.glide:glide:4.12.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
 
     implementation("androidx.core:core-splashscreen:1.0.1")
 }
