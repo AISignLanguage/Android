3f239482d97229b46715e0c0250de3efde631725
jiwon <pjw99032986@gmail.com>
2024-01-15 15:25:52 +0900

    id 화면추가

---
+++ app/src/main/java/com/example/ai_language/KaKaoLoginActivity.kt
@@ -0,0 +1,11 @@
+package com.example.ai_language
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class KaKaoLoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ka_kao_login)
+    }
+}
