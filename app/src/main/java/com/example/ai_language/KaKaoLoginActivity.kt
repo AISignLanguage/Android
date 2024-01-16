bc52c51d892805fd5fe79dc09b2d403dc0332038
shjin <shjin4667@gmail.com>
2024-01-17 01:04:03 +0900

    카카오 연동 완료

--- app/src/main/java/com/example/ai_language/KaKaoLoginActivity.kt
+++ app/src/main/java/com/example/ai_language/KaKaoLoginActivity.kt
@@ -20,12 +20,9 @@ class KaKaoLoginActivity : AppCompatActivity() {
         setContentView(R.layout.activity_ka_kao_login)
 
 
-
-
-
         val signInBtn = findViewById<TextView>(R.id.sign_in_button)
         signInBtn.setOnClickListener {
-            val intent = Intent(this,TermsActivity::class.java)
+            val intent = Intent(this, TermsActivity::class.java)
             startActivity(intent)
             finish()
         }
@@ -38,90 +35,71 @@ class KaKaoLoginActivity : AppCompatActivity() {
     }
 
 
-
-
-    private fun kakaoLogin(ctxt : Context) {
+    private fun kakaoLogin(ctxt: Context) {
+        val intent = Intent(ctxt, RegisterActivity::class.java)
         // 카카오계정으로 로그인 공통 callback 구성
         // 카카오톡으로 로그인 할 수 없어 카카오계정으로 로그인할 경우 사용됨
         val callback: (OAuthToken?, Throwable?) -> Unit = { token, error ->
             if (error != null) {
                 Log.e("결과", "카카오계정으로 로그인 실패 : ${error}")
+
             } else if (token != null) {
-                //TODO: 최종적으로 카카오로그인 및 유저정보 가져온 결과
-                UserApiClient.instance.me { user, error ->
-                    if (error != null) {
-                        Log.e("결과", "사용자 정보 요청 실패: ${error}")
-                    } else if (user != null) {
-                        val nickname = user.kakaoAccount?.profile?.nickname
-                        val profileImageUrl = user.kakaoAccount?.profile?.thumbnailImageUrl
-                        Log.d("결과", "닉네임: $nickname")
-                        Log.d("결과", "프로필 사진 URL: $profileImageUrl")
-
-                    }
-                }
-
+                requestUserInfoAndStartRegisterActivity(ctxt)
             }
         }
 
         // 카카오톡이 설치되어 있으면 카카오톡으로 로그인, 아니면 카카오계정으로 로그인
         if (UserApiClient.instance.isKakaoTalkLoginAvailable(this)) {
-            UserApiClient.instance.loginWithKakaoTalk(this) { token
-                                                              , error ->
+            UserApiClient.instance.loginWithKakaoTalk(this) { token, error ->
                 if (error != null) {
-                    Log.d("결과", "로그인 실패", error)
-                    if (error is ClientError && error.reason == ClientErrorCause.Cancelled) {
-                        return@loginWithKakaoTalk
-                    }
-// 카카오톡에 연결된 카카오계정이 없는 경우, 카카오계정으로 로그인 시도
+                    Log.d("결과", "카카오톡 로그인 실패", error)
                     UserApiClient.instance.loginWithKakaoAccount(this, callback = callback)
                 } else if (token != null) {
-                    Log.e("결과", "로그인 성공")
-                    // 사용자 정보 요청
-                    UserApiClient.instance.me { user, error ->
-                        if (error != null) {
-                            Log.e("결과", "사용자 정보 요청 실패: ${error}")
-                        } else if (user != null) {
-                            val nickname = user.kakaoAccount?.profile?.nickname
-                            val profileImageUrl = user.kakaoAccount?.profile?.thumbnailImageUrl
-                            Log.d("결과", "닉네임: $nickname")
-                            Log.d("결과", "프로필 사진 URL: $profileImageUrl")
-                        }
-                    }
-
-                    val kakaoBtn = findViewById<ImageView>(R.id.kko_login_btn)
-                    kakaoBtn.setBackgroundResource(R.drawable.logoutkakao)
-                    val intent = Intent(ctxt,TermsActivity::class.java)
-                    startActivity(intent)
+                    requestUserInfoAndStartRegisterActivity(ctxt)
                 }
             }
-
         } else {
             UserApiClient.instance.loginWithKakaoAccount(this, callback = callback)
         }
     }
 
-    private fun kakaoLogout(){
+    private fun kakaoLogout() {
         // 로그아웃
         UserApiClient.instance.logout { error ->
             if (error != null) {
                 Log.e("결과", "로그아웃 실패. SDK에서 토큰 삭제됨: ${error}")
-            }
-            else {
+            } else {
                 Log.e("결과", "로그아웃 성공. SDK에서 토큰 삭제됨")
             }
         }
     }
 
-    private fun kakaoUnlink(){
+    private fun kakaoUnlink() {
         // 연결 끊기
         UserApiClient.instance.unlink { error ->
             if (error != null) {
                 Log.e("결과", "연결 끊기 실패: ${error}")
-            }
-            else {
+            } else {
                 Log.e("결과", "연결 끊기 성공. SDK에서 토큰 삭제 됨")
             }
         }
     }
 
+    private fun requestUserInfoAndStartRegisterActivity(ctxt: Context) {
+        UserApiClient.instance.me { user, error ->
+            if (error != null) {
+                Log.e("결과", "사용자 정보 요청 실패: ${error}")
+            } else if (user != null) {
+                val nickname = user.kakaoAccount?.profile?.nickname
+                val profileImageUrl = user.kakaoAccount?.profile?.thumbnailImageUrl
+                Log.d("결과", "닉네임: $nickname, 프로필 사진 URL: $profileImageUrl")
+
+                val intent = Intent(ctxt, RegisterActivity::class.java).apply {
+                    putExtra("nick", nickname)
+                    putExtra("profile", profileImageUrl)
+                }
+                ctxt.startActivity(intent)
+            }
+        }
+    }
 }
--- app/src/main/java/com/example/ai_language/RegisterActivity.kt
+++ app/src/main/java/com/example/ai_language/RegisterActivity.kt
@@ -1,11 +1,46 @@
 package com.example.ai_language
-
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.TextView
+import com.bumptech.glide.Glide
 
 class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+
+        val nick = intent.getStringExtra("nick")
+
+        val regName = findViewById<EditText>(R.id.reg_name)
+        regName.setText(nick)
+
+        val profile = findViewById<ImageView>(R.id.reg_pro)
+        val uriString: String? = intent.getStringExtra("profile")
+            if (uriString != null) {
+
+                val profilePx = dpToPx(this, 90)
+
+                Glide.with(this)
+                    .load(uriString)
+                    .override(profilePx,profilePx)
+                    .into(profile)
+            } else {
+               Log.e("uri 에러","$uriString")
+            }
+
+        val regNext = findViewById<TextView>(R.id.reg_next)
+        regNext.setOnClickListener {
+            val intent = Intent(this,TermsActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+    private fun dpToPx(context: Context, dp: Int): Int {
+        return (dp * context.resources.displayMetrics.density).toInt()
     }
 }
