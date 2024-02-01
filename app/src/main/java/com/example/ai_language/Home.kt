c5b24f7969059c057b04898e5d6b826e7de46a0b
jiwon <pjw99032986@gmail.com>
2024-02-01 18:33:52 +0900

    오류해결

--- app/src/main/java/com/example/ai_language/Home.kt
+++ app/src/main/java/com/example/ai_language/Home.kt
@@ -1,13 +1,20 @@
 package com.example.ai_language
 
+import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.DisplayMetrics
+import android.util.Log
+import android.view.WindowInsets
+import android.view.WindowManager
 import android.widget.ImageButton
 import android.widget.Toast
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import androidx.core.view.updateLayoutParams
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import androidx.viewpager2.adapter.FragmentStateAdapter
@@ -40,6 +47,43 @@ class Home : AppCompatActivity() {
 
     private val CAMERA_PERMISSION_CODE = 1000
     private val READ_CONTACTS_PERMISSION_REQUEST = 1
+
+
+    // * 동적 화면 구성을 위한 확장 함수
+    // * 현재 화면의 가로 길이 반환
+    fun Context.getScreenWidth(): Int {
+        val wm = getSystemService(Context.WINDOW_SERVICE) as WindowManager
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            val windowMetrics = wm.currentWindowMetrics
+            val insets = windowMetrics.windowInsets
+                .getInsetsIgnoringVisibility(WindowInsets.Type.systemBars())
+            windowMetrics.bounds.width() - insets.left - insets.right
+        } else {
+            val displayMetrics = DisplayMetrics()
+            wm.defaultDisplay.getMetrics(displayMetrics)
+            displayMetrics.widthPixels
+        }
+    }
+
+    // * 현재 화면의 세로 길이 반환
+    fun Context.getScreenHeight(): Int {
+        val wm = getSystemService(Context.WINDOW_SERVICE) as WindowManager
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            val windowMetrics = wm.currentWindowMetrics
+            val insets = windowMetrics.windowInsets
+                .getInsetsIgnoringVisibility(WindowInsets.Type.systemBars())
+            windowMetrics.bounds.height() - insets.bottom - insets.top
+        } else {
+            val displayMetrics = DisplayMetrics()
+            wm.defaultDisplay.getMetrics(displayMetrics)
+            displayMetrics.heightPixels
+        }
+    }
+
+    private var sWidth : Int = 0
+    private var sHeight : Int = 0
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
@@ -94,6 +138,10 @@ class Home : AppCompatActivity() {
         }
 
         val callLsit_btn = findViewById<ImageButton>(R.id.callList_btn) //전화번호부 화면으로 이동
+//        callLsit_btn.updateLayoutParams{
+//            width = 100;
+//            height = 100;
+//        }
         callLsit_btn.setOnClickListener {
             val callPermission = ContextCompat.checkSelfPermission(this, android.Manifest.permission.READ_CONTACTS)
             if(callPermission != PackageManager.PERMISSION_GRANTED) {
