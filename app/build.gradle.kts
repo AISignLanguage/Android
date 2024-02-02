7b0d8dde6730920c3a1da0f9a248ab5bbbd0d035
jiwon <pjw99032986@gmail.com>
2024-02-02 14:37:00 +0900

    기기별 해상도대응

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,7 +38,6 @@ android {
 }
 
 dependencies {
-
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
@@ -47,13 +46,11 @@ dependencies {
     implementation ("me.relex:circleindicator:2.1.6")
     implementation ("com.github.bumptech.glide:glide:4.12.0")
     implementation("com.google.android.gms:play-services-wearable:18.1.0")
-
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     implementation ("com.kakao.sdk:v2-template:2.19.0") // 카카오톡 메시지 API
     implementation ("com.kakao.sdk:v2-link:2.5.3") // 카카오톡 메시지 링크 API
-
     implementation ("com.kakao.sdk:v2-all:2.19.0") // 전체 모듈 설치, 2.11.0 버전부터 지원
     implementation ("com.kakao.sdk:v2-user:2.19.0") // 카카오 로그인
     implementation ("com.kakao.sdk:v2-talk:2.19.0")// 친구, 메시지(카카오톡)
@@ -63,8 +60,7 @@ dependencies {
     implementation ("com.kakao.sdk:v2-cert:2.19.0") // 카카오 인증서비스
     implementation("androidx.core:core-splashscreen:1.0.1")
     implementation ("com.squareup.okhttp3:okhttp:4.9.2")
-    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.4")
-
+    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.1")
     //JetPack CameraX API
     var camerax_version = ("1.1.0-beta01")
     implementation ("androidx.camera:camera-core:${camerax_version}")
@@ -73,7 +69,6 @@ dependencies {
     implementation ("androidx.camera:camera-video:${camerax_version}")
     implementation ("androidx.camera:camera-view:${camerax_version}")
     implementation ("androidx.camera:camera-extensions:${camerax_version}")
-
     // ViewModel
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.3.1")
     // LiveData
