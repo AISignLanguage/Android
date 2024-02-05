12141775c6db69fb3c923dc31bd7cbcd7c9d359b
Seongmin <arcn1345@gmail.com>
2024-02-05 15:52:40 +0900

    dto

--- app/src/main/java/com/example/ai_language/Retrofit.kt
+++ app/src/main/java/com/example/ai_language/Retrofit.kt
@@ -12,7 +12,7 @@ class RetrofitClient private constructor() {
         @Volatile
         private var instance: RetrofitClient? = null
         private lateinit var userRetrofitInterface: Service
-        private val baseUrl = "http://192.168.32.91:8080/"
+        private val baseUrl = "https://c736-2001-e60-879e-8682-dc6c-e223-5d7b-4b74.ngrok-free.app/"
 
         fun getInstance(): RetrofitClient {
             return instance ?: synchronized(this) {
