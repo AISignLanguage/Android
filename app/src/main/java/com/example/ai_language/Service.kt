dc7866e3b856a0567e2a84e99ea6d34525061971
김성민 <arcn1345@gmail.com>
2024-02-16 01:49:35 +0900

    callList, dto 수정

--- app/src/main/java/com/example/ai_language/Service.kt
+++ app/src/main/java/com/example/ai_language/Service.kt
@@ -17,7 +17,7 @@ interface Service {
     fun sendData(@Body data: UserDTO): Call<LoginCheckDTO> // 전송할 데이터와 응답 타입
 
     @POST("send-callList") //CallListPage "접속할 때" 서버에 데이터 전달해줌 (페이지 들어갈 때마다 갱신되도록)
-    fun sendCallData(@Body contactsList: PhoneDTO): Call<PhoneDTO>
+    fun sendCallData(@Body contactsList: PhoneNumberDTO): Call<PhoneListDTO>
 
     @POST("send-news")
     fun sendNewsData(@Body data: NewsDTO): Call<NewsDTO>
