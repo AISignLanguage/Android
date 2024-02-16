e3881c084fd09e3f5fcbd9f7505fd0375b9302b8
Seongmin <arcn1345@gmail.com>
2024-02-16 14:24:30 +0900

    핸드폰 번호 포맷

--- app/src/main/java/com/example/ai_language/CallListPage.kt
+++ app/src/main/java/com/example/ai_language/CallListPage.kt
@@ -25,6 +25,8 @@ class CallListPage : AppCompatActivity() {
     lateinit var call : Call<PhoneListDTO>
     lateinit var service: Service
     lateinit var phoneNumberDTO: PhoneNumberDTO
+    lateinit var appPhoneNumbers: List<String> //서버에서 가져온 번호만 저장하는 리스트
+    val contactListMap = mutableListOf<Pair<String, String>>() // 이름, 번호 map
 
     private val callViewModel: CallListViewModel by viewModels()
     private val inviteViewModel: InviteViewModel by viewModels()
@@ -92,6 +94,13 @@ class CallListPage : AppCompatActivity() {
         })
     }
 
+
+    private fun ChangePhoneNumber(number: String?): Boolean {
+        // 유효한 전화번호 패턴 확인 (010-1234-5678)
+        val regex = Regex("^010-\\d{3,4}-\\d{4}$")
+        return !number.isNullOrEmpty() && regex.matches(number)
+    }
+
     private fun getContacts() {
         // 연락처 전체 정보에 대한 쿼리 수행
         val cursor: Cursor? = contentResolver.query(
@@ -117,13 +126,43 @@ class CallListPage : AppCompatActivity() {
                 val numberColumnIndex = phoneCursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER)
                 if (numberColumnIndex != -1) {
                     val number: String? = if (numberColumnIndex != -1) phoneCursor.getString(numberColumnIndex) else null
-                    number?.let { phoneNumbers.add(it) } // 전화번호가 null이 아닌 경우 리스트에 추가
+                    var cleanedNumber: String? = null
+                //number?.let { phoneNumbers.add(it) } // 전화번호가 null이 아닌 경우 리스트에 추가
+                // if (name != null && number != null) {
+                //contactListMap.add(Pair(name, number)) // 이름과 번호를 함께 Pair로 묶어 리스트에 추가
+                    //}
+
+                    // "+"와 "82"로 시작하는 경우에는 국가 코드를 제거하고 0을 추가하여 번호 정리
+                    if (number != null && (number.startsWith("+82") || number.startsWith("82"))) {
+                        cleanedNumber = "0${number.substring(3).replace("-", "")}"
+                    } else {
+                        // "-"를 제거하고 번호 정리
+                        cleanedNumber = number?.replace("-", "")?.replace(Regex("[^0-9]"), "")
+                    }
+                    // 번호가 11자리인 경우만 처리
+                    if (cleanedNumber?.length == 11) {
+                        // 번호가 010으로 시작하는 경우와 그렇지 않은 경우에 대해 다르게 처리
+                        val formattedNumber = if (cleanedNumber.startsWith("010")) {
+                            cleanedNumber.replace(Regex("(\\d{3})(\\d{4})(\\d{4})"), "$1-$2-$3")
+                        } else {
+                            null // 010으로 시작하지 않는 경우에는 null 반환하여 처리하지 않음
+                        }
+                        // 번호가 010으로 시작하는 경우에만 처리 결과를 저장
+                        formattedNumber?.let {
+                            phoneNumbers.add(it) // 전화번호가 null이 아닌 경우 리스트에 추가
+                            if (name != null) {
+                                contactListMap.add(Pair(name, it)) // 이름과 번호를 함께 Pair로 묶어 리스트에 추가
+                            }
+                        }
+                    }
+
                 }
             }
             phoneCursor?.close()
 
             // 해당 연락처의 전화번호들을 DTO로 변환하여 리스트에 추가
             phoneNumberDTO = PhoneNumberDTO(phoneNumbers)
+            Log.d("로그", "phoneNumbers : ${phoneNumbers}")
 
         } //while 종료
         cursor?.close()
@@ -141,19 +180,34 @@ class CallListPage : AppCompatActivity() {
             override fun onResponse(call: Call<PhoneListDTO>, response: Response<PhoneListDTO>) {
                 if (response.isSuccessful) {
                     val PhoneListDTO = response.body() // 서버에서 받은 데이터
-                    Log.d("로그", "onCreate 응답 성공")
                     PhoneListDTO?.phones?.let { phones ->
                         for (phoneDTOList in phones) {
                             for (phoneDTO in phoneDTOList) {
-                                Log.d(
-                                    "로그",
-                                    "Name: ${phoneDTO.name}, PhoneNumber: ${phoneDTO.phoneNumbers}, ProfileImageUrl: ${phoneDTO.profileImageUrl}"
-                                )
+                                Log.d("로그", "${phoneDTO.name}, ${phoneDTO.phoneNumbers}, Url: ${phoneDTO.profileImageUrl}")
+                                val callListItem = CallListItem(phoneDTO.name, phoneDTO.phoneNumbers)
+                                callViewModel.addListItem(callListItem) // 뷰 모델에 서버에서 가져온 데이터 추가
                             }
+                            // 중첩된 리스트에 대해 이중 반복문을 사용하여 phoneNumber 추출
+                            appPhoneNumbers = phones
+                                .flatMap { it } // 중첩된 리스트를 하나의 리스트로 평탄화
+                                .mapNotNull { it.phoneNumbers } // 각 PhoneDTO에서 phoneNumber 추출하고 null 필터링
+                            Log.d("로그", "appPhoneNumbers : ${appPhoneNumbers}")
                         }
-                        // 받아온 데이터를 처리
-                        // 예: 뷰 모델에 연동하여 UI 업데이트 등 수행
                     }
+
+                    // 서버에서 받은 번호와 일치하지 않는 번호만 남겨두기 위한 필터링
+                    val filteredContactListMap = contactListMap.filter { contact ->
+                        !appPhoneNumbers.contains(contact.second) // appPhoneNumbers에 해당 번호가 없는 경우만 필터링
+                    }
+                    contactListMap.clear() // 기존의 데이터를 모두 지움
+                    contactListMap.addAll(filteredContactListMap) // 필터링된 데이터만 추가
+
+                    //filteredContactListMap의 모든 항목 반복해서 아이템 만들고 뷰 모델에 추가
+                    for (contact in filteredContactListMap) {
+                        val inviteListItem = InviteListItem(contact.first, contact.second)
+                        inviteViewModel.addListItem(inviteListItem)
+                    }
+
                 } else {
                     // 요청 실패 처리
                     Log.d("로그", "데이터 요청 실패. 응답 코드: ${response.code()}, "
--- app/src/main/java/com/example/ai_language/CallListViewModel.kt
+++ app/src/main/java/com/example/ai_language/CallListViewModel.kt
@@ -33,13 +33,13 @@ class InviteViewModel : ViewModel() {
     var _inviteDataList = MutableLiveData<List<InviteListItem>>()
     val inviteDataList : LiveData<List<InviteListItem>>
         get() = _inviteDataList
-    init {
-        _inviteDataList.value = listOf(
-            InviteListItem("방경원", "010-8219-5021"),
-            InviteListItem("임멍멍", "010-1523-8445"),
-            InviteListItem("신정인", "010-1209-0084"),
-        )
-    }
+//    init {
+//        _inviteDataList.value = listOf(
+//            InviteListItem("방경원", "010-8219-5021"),
+//            InviteListItem("임멍멍", "010-1523-8445"),
+//            InviteListItem("신정인", "010-1209-0084"),
+//        )
+//    }
     fun addListItem(item: InviteListItem) {
         //liveData가 비었으면 빈 mutableList 생성후 넣음, 아니면 그냥 넣음
         val currentList = inviteDataList.value?.toMutableList() ?: mutableListOf()
