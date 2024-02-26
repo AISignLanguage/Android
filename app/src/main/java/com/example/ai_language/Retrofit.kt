3770159e73343f2f106a2465dbf108f748f6c475
jiwon <pjw99032986@gmail.com>
2024-02-26 21:42:56 +0900

    카카오레지스터 수정

--- app/src/main/java/com/example/ai_language/Retrofit.kt
+++ app/src/main/java/com/example/ai_language/Retrofit.kt
@@ -13,7 +13,7 @@ class RetrofitClient private constructor() {
         @Volatile
         private var instance: RetrofitClient? = null
         private lateinit var userRetrofitInterface: Service
-        private val baseUrl = "http://192.168.219.100:8080/api/mog/user/"
+        private val baseUrl = "http://34.64.212.107:8080/api/mog/user/"
         //private val baseUrl = "http://192.168.1.121:8080/api/mog/user/" - 지원
 
         fun getInstance(): RetrofitClient {
