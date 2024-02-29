8962fb4627245787ce14dc757c02a44633d5f86a
Seongmin <arcn1345@gmail.com>
2024-02-29 18:07:39 +0900

    아이디/비번 찾기 프래그먼트

--- app/src/main/java/com/example/ai_language/IdFindFragment.kt
+++ app/src/main/java/com/example/ai_language/IdFindFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
 
 class IdFindFragment : Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,6 +16,10 @@ class IdFindFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_id_find, container, false)
+    }
+
+    fun findId() {
+
     }
 
 }
--- app/src/main/java/com/example/ai_language/PasswordFindFragment.kt
+++ app/src/main/java/com/example/ai_language/PasswordFindFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
+import android.widget.TextView
 
 class PasswordFindFragment : Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,4 +18,13 @@ class PasswordFindFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_password_find, container, false)
     }
 
+    fun findPassword() {
+        val id_edit = view?.findViewById<EditText>(R.id.find_id)
+        val certification_edit = view?.findViewById<EditText>(R.id.certification_et)
+        val find_btn = view?.findViewById<TextView>(R.id.find_id_btn)
+
+        find_btn?.setOnClickListener {
+
+        }
+    }
 }
