b36a360b70dc3a7c9360f7195122a130db00a5cf
shjin <shjin4667@gmail.com>
2024-02-29 18:26:19 +0900

    show dialog

--- app/src/main/java/com/example/ai_language/ChangePw.kt
+++ app/src/main/java/com/example/ai_language/ChangePw.kt
@@ -1,11 +1,24 @@
 package com.example.ai_language
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.widget.AppCompatButton
 
 class ChangePw : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_change_pw)
+
+        val checkButton = findViewById<AppCompatButton>(R.id.checkButton)
+
+        checkButton.setOnClickListener {
+            val dialog = ChangePwDialog(this)
+            dialog.show()
+        }
+
+
     }
 }
--- app/src/main/java/com/example/ai_language/ChangePwDialog.kt
+++ app/src/main/java/com/example/ai_language/ChangePwDialog.kt
@@ -1,20 +1,22 @@
 package com.example.ai_language
 
+import android.app.Dialog
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.view.LayoutInflater
+import android.view.Window
 
-class ChangePwDialog : AppCompatActivity() {
+class ChangePwDialog (context: Context): Dialog(context) {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
         setContentView(R.layout.activity_change_pw_dialog)
-
-        Handler(Looper.getMainLooper()).postDelayed({
-            val intent = Intent(this, LoginActivity::class.java)
-            startActivity(intent)
-            finish()
-        }, 2000)
+        show()
     }
+
 }
