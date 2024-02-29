9f6ffce66f25a2420f38669ae6a69d39b3b83fd0
shjin <shjin4667@gmail.com>
2024-02-29 17:50:01 +0900

    change pw dialog

--- app/src/main/java/com/example/ai_language/ChangePwDialog.kt
+++ app/src/main/java/com/example/ai_language/ChangePwDialog.kt
@@ -1,11 +1,20 @@
 package com.example.ai_language
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 
 class ChangePwDialog : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_change_pw_dialog)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 2000)
     }
 }
