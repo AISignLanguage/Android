d20fdb411fa91680274d6053ae83e33d88b8740a
shjin <shjin4667@gmail.com>
2024-03-08 02:41:01 +0900

    탈퇴 사유 스피너

--- app/src/main/java/com/example/ai_language/Unregister.kt
+++ app/src/main/java/com/example/ai_language/Unregister.kt
@@ -11,6 +11,7 @@ import android.view.ViewGroup
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.Button
+import android.widget.EditText
 import android.widget.ImageButton
 import android.widget.Spinner
 import android.widget.TextView
@@ -33,10 +34,12 @@ class Unregister : AppCompatActivity() {
 
         val items = resources.getStringArray(R.array.unregister)
         val spinner = findViewById<Spinner>(R.id.spinner)
+        val editText = findViewById<EditText>(R.id.editText)
 
         val adapter = object : ArrayAdapter<String>(this, R.layout.spinner_item_layout, items.toMutableList()) {
             override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
                 val v = super.getView(position, convertView, parent)
+                val editText = findViewById<EditText>(R.id.editText)
                 if (position == 0) {
                     val textView = v.findViewById<TextView>(R.id.tvItemSpinner)
                     textView.text = ""
@@ -52,18 +55,31 @@ class Unregister : AppCompatActivity() {
 
         spinner.adapter = adapter
 
+        val unregisterButton = findViewById<Button>(R.id.unregisterButton)
+        unregisterButton.paintFlags = Paint.UNDERLINE_TEXT_FLAG
+
         spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
             override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
                 val selectedItem = items[position]
                 Log.d("Selected Item", selectedItem)
+                if (position == 0) {
+                    unregisterButton.isEnabled = false
+                    unregisterButton.setBackgroundResource(R.drawable.unregister_btn2)
+                } else {
+                    unregisterButton.isEnabled = true
+                    unregisterButton.setBackgroundResource(R.drawable.unregister_btn)
+                    if (position == 5) {
+                        editText.visibility = View.VISIBLE
+                    } else {
+                        editText.visibility = View.GONE
+                    }
+                }
             }
 
             override fun onNothingSelected(parent: AdapterView<*>?) {
             }
         }
 
-        val unregisterButton = findViewById<Button>(R.id.unregisterButton)
-        unregisterButton.paintFlags = Paint.UNDERLINE_TEXT_FLAG
         unregisterButton.setOnClickListener{
             userDelete()
             val unregisterDialog = UnregisterDialog(this)
