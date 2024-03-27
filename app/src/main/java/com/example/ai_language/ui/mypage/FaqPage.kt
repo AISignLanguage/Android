1603f7fe7fc24c92219ebce72ab136c57b57ab11
jiwon <pjw99032986@gmail.com>
2024-03-27 18:23:05 +0900

    refactor : MyPage.kt 이전

--- app/src/main/java/com/example/ai_language/ui/mypage/FaqPage.kt
+++ app/src/main/java/com/example/ai_language/ui/mypage/FaqPage.kt
@@ -1,11 +1,11 @@
-package com.example.ai_language
+package com.example.ai_language.ui.mypage
 
 import android.os.Bundle
 import android.view.View
-import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.example.ai_language.R
 
 class FaqPage : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,7 +16,7 @@ class FaqPage : AppCompatActivity() {
         val question1 = findViewById<TextView>(R.id.question1)
         val answer1 = findViewById<LinearLayout>(R.id.answer1)
         val questionLayout1 = findViewById<LinearLayout>(R.id.questionLayout1)
-        question1.setOnClickListener{
+        question1.setOnClickListener {
             if (answer1.visibility == View.VISIBLE) {
                 answer1.visibility = View.GONE
                 questionLayout1.setBackgroundResource(R.drawable.question);
@@ -30,7 +30,7 @@ class FaqPage : AppCompatActivity() {
         val question2 = findViewById<TextView>(R.id.question2)
         val answer2 = findViewById<LinearLayout>(R.id.answer2)
         val questionLayout2 = findViewById<LinearLayout>(R.id.questionLayout2)
-        question2.setOnClickListener{
+        question2.setOnClickListener {
             if (answer2.visibility == View.VISIBLE) {
                 answer2.visibility = View.GONE
                 questionLayout2.setBackgroundResource(R.drawable.question);
@@ -44,7 +44,7 @@ class FaqPage : AppCompatActivity() {
         val question3 = findViewById<TextView>(R.id.question3)
         val answer3 = findViewById<LinearLayout>(R.id.answer3)
         val questionLayout3 = findViewById<LinearLayout>(R.id.questionLayout3)
-        question3.setOnClickListener{
+        question3.setOnClickListener {
             if (answer3.visibility == View.VISIBLE) {
                 answer3.visibility = View.GONE
                 questionLayout3.setBackgroundResource(R.drawable.question);
@@ -58,7 +58,7 @@ class FaqPage : AppCompatActivity() {
         val question4 = findViewById<TextView>(R.id.question4)
         val answer4 = findViewById<LinearLayout>(R.id.answer4)
         val questionLayout4 = findViewById<LinearLayout>(R.id.questionLayout4)
-        question4.setOnClickListener{
+        question4.setOnClickListener {
             if (answer4.visibility == View.VISIBLE) {
                 answer4.visibility = View.GONE
                 questionLayout4.setBackgroundResource(R.drawable.question);
@@ -72,7 +72,7 @@ class FaqPage : AppCompatActivity() {
         val question5 = findViewById<TextView>(R.id.question5)
         val answer5 = findViewById<LinearLayout>(R.id.answer5)
         val questionLayout5 = findViewById<LinearLayout>(R.id.questionLayout5)
-        question5.setOnClickListener{
+        question5.setOnClickListener {
             if (answer5.visibility == View.VISIBLE) {
                 answer5.visibility = View.GONE
                 questionLayout5.setBackgroundResource(R.drawable.question);
@@ -86,7 +86,7 @@ class FaqPage : AppCompatActivity() {
         val question6 = findViewById<TextView>(R.id.question6)
         val answer6 = findViewById<LinearLayout>(R.id.answer6)
         val questionLayout6 = findViewById<LinearLayout>(R.id.questionLayout6)
-        question6.setOnClickListener{
+        question6.setOnClickListener {
             if (answer6.visibility == View.VISIBLE) {
                 answer6.visibility = View.GONE
                 questionLayout6.setBackgroundResource(R.drawable.question);
@@ -100,7 +100,7 @@ class FaqPage : AppCompatActivity() {
         val question7 = findViewById<TextView>(R.id.question7)
         val answer7 = findViewById<LinearLayout>(R.id.answer7)
         val questionLayout7 = findViewById<LinearLayout>(R.id.questionLayout7)
-        question7.setOnClickListener{
+        question7.setOnClickListener {
             if (answer7.visibility == View.VISIBLE) {
                 answer7.visibility = View.GONE
                 questionLayout7.setBackgroundResource(R.drawable.question);
