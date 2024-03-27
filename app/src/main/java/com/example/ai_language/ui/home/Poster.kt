0f3a144895a584815c8a9f272ce18708e5c14db3
jiwon <pjw99032986@gmail.com>
2024-03-27 18:27:41 +0900

    refactor : Home 이전

--- app/src/main/java/com/example/ai_language/ui/home/Poster.kt
+++ app/src/main/java/com/example/ai_language/ui/home/Poster.kt
@@ -1,7 +1,8 @@
-package com.example.ai_language
+package com.example.ai_language.ui.home
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.ai_language.R
 
 class Poster : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
