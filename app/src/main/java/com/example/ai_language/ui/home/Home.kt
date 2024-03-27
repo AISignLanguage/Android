58dc332c1445a0b60d016d4504c1c62fe182f57c
jiwon <pjw99032986@gmail.com>
2024-03-27 18:24:05 +0900

    refactor : Home 이전

--- app/src/main/java/com/example/ai_language/ui/home/Home.kt
+++ app/src/main/java/com/example/ai_language/ui/home/Home.kt
@@ -1,4 +1,4 @@
-package com.example.ai_language
+package com.example.ai_language.ui.home
 
 import android.content.Intent
 import android.content.pm.PackageManager
@@ -11,30 +11,15 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import androidx.viewpager2.adapter.FragmentStateAdapter
 import androidx.viewpager2.widget.ViewPager2
-import com.example.ai_language.call.CallListPage
-import com.example.ai_language.camera.CameraPage
+import com.example.ai_language.ui.mypage.MyPage
+import com.example.ai_language.ui.news.NewsActivity
+import com.example.ai_language.R
+import com.example.ai_language.data.remote.Service
+import com.example.ai_language.ui.call.CallListPage
+import com.example.ai_language.ui.camera.CameraPage
+import com.example.ai_language.ui.dictionary.DictionaryPage
+import com.example.ai_language.ui.home.adapter.PagerAdapter
 import me.relex.circleindicator.CircleIndicator3
-
-
-class PagerAdapter(fa: FragmentActivity, private val mCount: Int) : FragmentStateAdapter(fa) {
-
-    override fun createFragment(position: Int): Fragment {
-
-        return when (getRealPosition(position)) {
-            0 -> Page1()
-            1 -> Page2()
-            else -> Page3()
-        }
-    }
-
-    override fun getItemCount(): Int {
-        return 2000
-    }
-
-    private fun getRealPosition(position: Int): Int {
-        return position % mCount
-    }
-}
 
 
 class Home : AppCompatActivity() {
@@ -50,24 +35,29 @@ class Home : AppCompatActivity() {
 
         val page = 3
         val viewPager2 = findViewById<ViewPager2>(R.id.viewpager)
-        val pagerAdapter = PagerAdapter(this,page)
+        val pagerAdapter = PagerAdapter(this, page)
         viewPager2.adapter = pagerAdapter
 
         val indicator = findViewById<CircleIndicator3>(R.id.indicator)
         indicator.setViewPager(viewPager2)
-        indicator.createIndicators(page,0)
+        indicator.createIndicators(page, 0)
         viewPager2.orientation = ViewPager2.ORIENTATION_HORIZONTAL
 
         viewPager2.currentItem = 1000
         viewPager2.offscreenPageLimit = 3
 
         viewPager2.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
-            override fun onPageScrolled(position: Int, positionOffset: Float, positionOffsetPixels: Int) {
+            override fun onPageScrolled(
+                position: Int,
+                positionOffset: Float,
+                positionOffsetPixels: Int
+            ) {
                 super.onPageScrolled(position, positionOffset, positionOffsetPixels)
                 if (positionOffsetPixels == 0) {
                     viewPager2.currentItem = position
                 }
             }
+
             override fun onPageSelected(position: Int) {
                 super.onPageSelected(position)
                 val thispage = position % page;
@@ -81,13 +71,13 @@ class Home : AppCompatActivity() {
 
         val dic_btn = findViewById<ImageButton>(R.id.dic_btn)
         dic_btn.setOnClickListener {
-            val intent = Intent(this,DictionaryPage::class.java)
+            val intent = Intent(this, DictionaryPage::class.java)
             startActivity(intent)
         }
 
         val mypg_btn = findViewById<ImageButton>(R.id.my_page_btn)
-        mypg_btn.setOnClickListener{
-            val intent = Intent(this,MyPage::class.java)
+        mypg_btn.setOnClickListener {
+            val intent = Intent(this, MyPage::class.java)
             startActivity(intent)
         }
 
@@ -121,31 +111,31 @@ class Home : AppCompatActivity() {
         val callLsit_btn = findViewById<ImageButton>(R.id.callList_btn) //전화번호부 화면으로 이동
 
         callLsit_btn.setOnClickListener {
-            val callPermission = ContextCompat.checkSelfPermission(this, android.Manifest.permission.READ_CONTACTS)
-            if(callPermission != PackageManager.PERMISSION_GRANTED) {
+            val callPermission =
+                ContextCompat.checkSelfPermission(this, android.Manifest.permission.READ_CONTACTS)
+            if (callPermission != PackageManager.PERMISSION_GRANTED) {
                 ActivityCompat.requestPermissions(
                     this,
                     arrayOf(android.Manifest.permission.READ_CONTACTS),
                     READ_CONTACTS_PERMISSION_REQUEST
                 )
-            }
-            else{
+            } else {
                 val intent = Intent(this, CallListPage::class.java)
                 startActivity(intent)
             }
         }
 
         val camera_btn = findViewById<ImageButton>(R.id.CameraBtn) //카메라(동영상) 화면으로 이동
-        camera_btn.setOnClickListener{
-            val cameraPermission = ContextCompat.checkSelfPermission(this, android.Manifest.permission.CAMERA)
+        camera_btn.setOnClickListener {
+            val cameraPermission =
+                ContextCompat.checkSelfPermission(this, android.Manifest.permission.CAMERA)
             if (cameraPermission != PackageManager.PERMISSION_GRANTED) {
                 ActivityCompat.requestPermissions(
                     this,
                     arrayOf(android.Manifest.permission.CAMERA),
                     CAMERA_PERMISSION_CODE
                 )
-            }
-            else{
+            } else {
                 val intent = Intent(this, CameraPage::class.java)
                 startActivity(intent)
             }
