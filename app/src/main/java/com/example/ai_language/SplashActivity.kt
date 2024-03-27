3784a6e7fe798ae1b3186a2c511ae49c58c917f3
jiwon <pjw99032986@gmail.com>
2024-03-27 18:28:37 +0900

    refactor : Splash 이전

--- app/src/main/java/com/example/ai_language/SplashActivity.kt
+++ app/src/main/java/com/example/ai_language/SplashActivity.kt
@@ -4,15 +4,8 @@ import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
-import android.util.Log
-import android.view.View
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.kakao.sdk.common.KakaoSdk
-import com.kakao.sdk.common.util.Utility
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
+import com.example.ai_language.ui.account.LoginActivity
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
