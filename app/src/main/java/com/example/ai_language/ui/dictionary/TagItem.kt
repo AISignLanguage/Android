a4ff5b49f81f7bb82ce4681763d603135c5ef373
jiwon <pjw99032986@gmail.com>
2024-03-27 18:29:53 +0900

    refactor : dictionary 이전

--- app/src/main/java/com/example/ai_language/ui/dictionary/TagItem.kt
+++ app/src/main/java/com/example/ai_language/ui/dictionary/TagItem.kt
@@ -1,14 +1,13 @@
-package com.example.ai_language
+package com.example.ai_language.ui.dictionary
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
+import com.example.ai_language.R
 
 class TagItem : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_tag_item)
-
 
 
     }
