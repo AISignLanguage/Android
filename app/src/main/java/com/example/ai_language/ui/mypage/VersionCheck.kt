e7682beae5804216e50b21509882b40e56752a44
jiwon <pjw99032986@gmail.com>
2024-03-27 18:30:32 +0900

    feat : Mypage 추가

--- app/src/main/java/com/example/ai_language/ui/mypage/VersionCheck.kt
+++ app/src/main/java/com/example/ai_language/ui/mypage/VersionCheck.kt
@@ -1,4 +1,4 @@
-package com.example.ai_language
+package com.example.ai_language.ui.mypage
 
 import android.content.Intent
 import android.graphics.Paint
@@ -6,6 +6,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageButton
 import android.widget.TextView
+import com.example.ai_language.R
+import com.example.ai_language.ui.home.Home
 
 class VersionCheck : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,8 +19,8 @@ class VersionCheck : AppCompatActivity() {
         //update.setOnClickListener {  } //이후 추가
 
         val homeButton = findViewById<ImageButton>(R.id.homeButton4)
-        homeButton.setOnClickListener{
-            val intent = Intent(this,Home::class.java)
+        homeButton.setOnClickListener {
+            val intent = Intent(this, Home::class.java)
             startActivity(intent)
             finish()
         }
