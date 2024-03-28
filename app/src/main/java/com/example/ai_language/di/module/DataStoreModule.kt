298ad5747b6fe5432a3789f6c1e720fcf84a622c
jiwon <pjw99032986@gmail.com>
2024-03-29 03:35:28 +0900

    refactor : 데이터 스토어 모듈 추가

--- app/src/main/java/com/example/ai_language/di/module/DataStoreModule.kt
+++ app/src/main/java/com/example/ai_language/di/module/DataStoreModule.kt
@@ -1,21 +1,22 @@
-//package com.example.ai_language.di.module
-//
-//import android.content.Context
-//import com.goorm.kkiri.util.TokenManager
-//import dagger.Module
-//import dagger.Provides
-//import dagger.hilt.InstallIn
-//import dagger.hilt.android.qualifiers.ApplicationContext
-//import dagger.hilt.components.SingletonComponent
-//import javax.inject.Singleton
-//
-//@Module
-//@InstallIn(SingletonComponent::class)
-//object DataStoreModule {
-//
-//    @Singleton
-//    @Provides
-//    fun provideTokenManager(@ApplicationContext context: Context): TokenManager {
-//        return TokenManager(context)
-//    }
-//}
+package com.example.ai_language.di.module
+
+import android.content.Context
+import com.example.ai_language.Util.TokenManager
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+
+@Module
+@InstallIn(SingletonComponent::class)
+object DataStoreModule {
+
+    @Singleton
+    @Provides
+    fun provideTokenManager(@ApplicationContext context: Context): TokenManager {
+        return TokenManager(context)
+    }
+}
