cd7e3b597c360f8d902b53432692299563fcd1ed
jiwon <pjw99032986@gmail.com>
2024-03-29 00:57:04 +0900

    feat : 플로팅 버튼 액션 추가

--- app/src/main/java/com/example/ai_language/ui/home/Home.kt
+++ app/src/main/java/com/example/ai_language/ui/home/Home.kt
@@ -1,35 +1,21 @@
 package com.example.ai_language.ui.home
 
+
 import android.content.Intent
-import android.content.pm.PackageManager
-import android.os.Bundle
-import android.widget.ImageButton
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
-import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentActivity
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.setupWithNavController
-import androidx.viewpager2.adapter.FragmentStateAdapter
-import androidx.viewpager2.widget.ViewPager2
-import com.example.ai_language.ui.mypage.MyPage
-import com.example.ai_language.ui.news.NewsActivity
 import com.example.ai_language.R
 import com.example.ai_language.base.BaseActivity
 import com.example.ai_language.data.remote.Service
 import com.example.ai_language.databinding.ActivityHomeBinding
+import com.example.ai_language.ui.camera.CameraPage
 
 class Home : BaseActivity<ActivityHomeBinding>(R.layout.activity_home) {
 
-    private val CAMERA_PERMISSION_CODE = 1000
-    private val READ_CONTACTS_PERMISSION_REQUEST = 1
-    lateinit var service: Service
-
 
     override fun setLayout() {
-//        setButtonClickListeners()
         setBottomNavigation()
+        setOnClick()
     }
 
     private fun setBottomNavigation() {
@@ -39,7 +25,15 @@ class Home : BaseActivity<ActivityHomeBinding>(R.layout.activity_home) {
         navController?.let {
             bottomNavigationView.setupWithNavController(it)
         }
+
     }
+    fun setOnClick(){
+        binding.ftbCamera.setOnClickListener{
+            startActivity(Intent(this, CameraPage::class.java))
+
+        }
+    }
+
 
 
 
