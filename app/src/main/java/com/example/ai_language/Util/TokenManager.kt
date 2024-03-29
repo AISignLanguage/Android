79bd60588a29785a4e8edfb74dad74ebb2c9384c
jiwon <pjw99032986@gmail.com>
2024-03-30 03:29:15 +0900

    refactor : 데이터 스토어 토큰 매니저 함수 추가

--- app/src/main/java/com/example/ai_language/Util/TokenManager.kt
+++ app/src/main/java/com/example/ai_language/Util/TokenManager.kt
@@ -6,6 +6,7 @@ import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.core.edit
 import androidx.datastore.preferences.core.stringPreferencesKey
 import com.example.ai_language.Util.extensions.datastore
+import com.kakao.sdk.auth.model.OAuthToken
 import dagger.hilt.android.qualifiers.ApplicationContext
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
@@ -18,26 +19,37 @@ class TokenManager @Inject constructor(
 ) {
     private val dataStore: DataStore<Preferences> = context.datastore
 
-    val userIdFlow: Flow<String?> = dataStore.data
+
+    //토큰만 걸러서 뽑아줌
+    val findToken: Flow<String?> = dataStore.data
         .map { preferences ->
-            preferences[USER_ID]
+            preferences[ACCESS_TOKEN]
         }
 
     suspend fun deleteToken() {
         dataStore.edit { preferences ->
-            preferences.remove(USER_ID)
+            preferences.remove(ACCESS_TOKEN)
         }
     }
 
-    suspend fun saveUserID(userId: Long) {
-        if (userId >= 0) {
-            dataStore.edit { preferences ->
-                preferences[USER_ID] = userId.toString()
-            }
+    suspend fun saveToken(token: OAuthToken?) {
+        dataStore.edit { preferences ->
+            preferences[ACCESS_TOKEN] = token.toString()
         }
     }
 
+    fun getAccessToken(): Flow<String?> = dataStore.data
+        .map { preferences ->
+            preferences[ACCESS_TOKEN]
+        }
+    fun getRefreshToken(): Flow<String?> = dataStore.data
+        .map { preferences ->
+            preferences[REFRESH_TOKEN]
+        }
     companion object {
         private val USER_ID = stringPreferencesKey("user_id")
+        private val ACCESS_TOKEN = stringPreferencesKey("access_token")
+        private val REFRESH_TOKEN = stringPreferencesKey("refresh_token")
+
     }
 }
