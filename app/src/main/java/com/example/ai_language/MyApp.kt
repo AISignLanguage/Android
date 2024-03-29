52b7209e0bab22fa924c56d58b4719ebd4a3833b
jiwon <pjw99032986@gmail.com>
2024-03-30 03:28:45 +0900

    refactor : 앱 초기화

--- app/src/main/java/com/example/ai_language/MyApp.kt
+++ app/src/main/java/com/example/ai_language/MyApp.kt
@@ -2,12 +2,17 @@ package com.example.ai_language
 
 import android.app.Application
 import android.util.Log
+import com.example.ai_language.Util.TokenManager
 import com.kakao.sdk.common.KakaoSdk
 import com.kakao.sdk.common.util.Utility
 import dagger.hilt.android.HiltAndroidApp
+import javax.inject.Inject
 
 @HiltAndroidApp
 class MyApp : Application() {
+
+    @Inject
+    lateinit var tokenManager: TokenManager
     companion object {
         lateinit var application: MyApp
 
@@ -18,13 +23,12 @@ class MyApp : Application() {
 
     override fun onCreate() {
         super.onCreate()
+        application = this
         // 카카오 SDK 초기화
         KakaoSdk.init(this, "ddef2e841959182411eab6098da996e5")
-
         // 해시키 로깅
         val keyHash = Utility.getKeyHash(this)
         Log.d("해시키", keyHash)
 
-        application = this
     }
 }
