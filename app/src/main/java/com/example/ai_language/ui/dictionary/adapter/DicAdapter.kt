0407090260cb6e0ac64c41f733c73d909e4f9abd
jiwon <pjw99032986@gmail.com>
2024-03-31 02:11:50 +0900

    feat : 어댑터 수정 (dic) , ResponseBodys로 리스트 변경

--- app/src/main/java/com/example/ai_language/ui/dictionary/adapter/DicAdapter.kt
+++ app/src/main/java/com/example/ai_language/ui/dictionary/adapter/DicAdapter.kt
@@ -1,65 +1,49 @@
 package com.example.ai_language.ui.dictionary.adapter
 
-import android.content.Context
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
-import com.bumptech.glide.Glide
-import com.bumptech.glide.load.resource.bitmap.RoundedCorners
-import com.example.ai_language.R
-import com.example.ai_language.ui.dictionary.viewmodel.DicPic
-
-class DicAdapter(val itemList: ArrayList<DicPic>, private val listener: OnItemClickListener) :
-    RecyclerView.Adapter<DicAdapter.DicViewHolder>() {
-    interface OnItemClickListener {
-        fun onItemClick(dicPic: DicPic)
+import com.example.ai_language.databinding.ActivityDicItemBinding
+import com.example.ai_language.domain.model.response.Item
+import com.example.ai_language.domain.model.response.ResponseBodys
+import com.example.ai_language.ui.dictionary.data.DicPic
+class DicAdapter : RecyclerView.Adapter<DicAdapter.DicViewHolder>() {
+    private val items = mutableListOf<Item>()
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DicViewHolder {
+        return DicViewHolder.from(parent)
     }
 
-    inner class DicViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        val dic_img = itemView.findViewById<ImageView>(R.id.dic_item)
-        val dic_ex = itemView.findViewById<TextView>(R.id.dic_text)
-
-        init {
-            itemView.setOnClickListener {
-                val position = adapterPosition
-                if (position != RecyclerView.NO_POSITION) {
-                    listener.onItemClick(itemList[position])
-                }
-            }
-        }
-
-    }
-
-    override fun onCreateViewHolder(
-        parent: ViewGroup,
-        viewType: Int
-    ): DicViewHolder {
-        val view =
-            LayoutInflater.from(parent.context).inflate(R.layout.activity_dic_item, parent, false)
-        return DicViewHolder(view)
-    }
-
-    private fun dpToPx(context: Context, dp: Int): Int {
-        return (dp * context.resources.displayMetrics.density).toInt()
-    }
+    override fun getItemCount(): Int = items.size
 
     override fun onBindViewHolder(holder: DicViewHolder, position: Int) {
-        val dicPic = itemList[position]
-        val dicUri = dicPic.dicpic
-        //holder.dic_img.setImageURI(dicUri)
-        val radiusInPixels = dpToPx(holder.itemView.context, 26)
-        holder.dic_ex.text = dicPic.ex
-        Glide.with(holder.itemView)
-            .load(dicUri)
-            .transform(RoundedCorners(radiusInPixels))
-            .centerCrop()
-            .into(holder.dic_img)
+        holder.bind(items[position])
     }
 
-    override fun getItemCount(): Int {
-        return itemList.size
+    fun update(newItems: ResponseBodys) {
+        val positionStart = newItems.response?.body?.items?.item?.size
+        newItems.response?.body?.items?.item?.let { items.addAll(it) }
+        if (positionStart != null) {
+            notifyItemRangeInserted(positionStart, items.size)
+        }
+    }
+
+    class DicViewHolder(
+        private val binding: ActivityDicItemBinding
+    ) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(item: Item) {
+            binding.dicPic = item
+        }
+
+        companion object {
+            fun from(parent: ViewGroup): DicViewHolder {
+                return DicViewHolder(
+                    ActivityDicItemBinding.inflate(
+                        LayoutInflater.from(parent.context),
+                        parent,
+                        false
+                    )
+                )
+            }
+        }
     }
 }
