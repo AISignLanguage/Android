b104204012737c77e9eac0a59d13bc873accb1b5
jiwon <pjw99032986@gmail.com>
2024-03-31 02:21:56 +0900

    feat : 사전 서비스 모듈 추가 및 OpenApi 전용 레트로핏으로 설정

--- app/src/main/java/com/example/ai_language/di/module/ServiceModule.kt
+++ app/src/main/java/com/example/ai_language/di/module/ServiceModule.kt
@@ -1,35 +1,23 @@
-//package com.example.ai_language.di.module
-//
-//import com.goorm.kkiri.data.remote.BoardService
-//import com.goorm.kkiri.data.remote.ChatService
-//import com.goorm.kkiri.data.remote.MemberService
-//import dagger.Module
-//import dagger.Provides
-//import dagger.hilt.InstallIn
-//import dagger.hilt.components.SingletonComponent
-//import retrofit2.Retrofit
-//import retrofit2.create
-//import javax.inject.Singleton
-//
-//@Module
-//@InstallIn(SingletonComponent::class)
-//object ServiceModule {
-//
-//    @Provides
-//    @Singleton
-//    fun provideMemberService(
-//        @NetworkModule.MogInterceptorOkHttpClient retrofit: Retrofit
-//    ): MemberService = retrofit.create(MemberService::class.java)
-//
-//    @Provides
-//    @Singleton
-//    fun provideBoardService(
-//        @NetworkModule.MogInterceptorOkHttpClient retrofit: Retrofit
-//    ): BoardService = retrofit.create(BoardService::class.java)
-//
-//    @Provides
-//    @Singleton
-//    fun provideChatService(
-//        @NetworkModule.MogInterceptorOkHttpClient retrofit: Retrofit
-//    ): ChatService = retrofit.create(ChatService::class.java)
-//}
+package com.example.ai_language.di.module
+
+import com.example.ai_language.data.remote.DictionaryService
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.create
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object ServiceModule {
+
+    @Provides
+    @Singleton
+    fun provideOpenApiService(
+        @NetworkModule.OpenApiRetrofit retrofit: Retrofit
+    ): DictionaryService = retrofit.create(DictionaryService::class.java)
+
+
+}
