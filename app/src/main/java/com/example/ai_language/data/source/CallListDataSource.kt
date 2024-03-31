b103a65f098f75c90adc5a3ddf226862151d456e
김성민 <arcn1345@gmail.com>
2024-04-01 01:13:56 +0900

    feat : CallListDataSource 데이터 소스 구현부 추가

--- app/src/main/java/com/example/ai_language/data/source/CallListDataSource.kt
+++ app/src/main/java/com/example/ai_language/data/source/CallListDataSource.kt
@@ -1,16 +1,21 @@
 package com.example.ai_language.data.source
 
-class CallListDataSource {
+import android.util.Log
+import com.example.ai_language.data.remote.CallListService
+import com.example.ai_language.domain.model.request.PhoneListDTO
+import com.example.ai_language.domain.model.request.PhoneNumberDTO
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.flow
+import javax.inject.Inject
 
+class CallListDataSource @Inject constructor(
+  private val callListService: CallListService
+) {
+    fun sendPhoneNumbers(phoneNumbers: PhoneNumberDTO): Flow<PhoneListDTO> = flow {
+        val result = callListService.sendPhoneNumbers(phoneNumbers)
+        emit(result)
+    }.catch {
+        Log.e("Post Call By sendPhoneNumbers Failure", it.message.toString())
+    }
 }
-
-//class DictionaryDataSource @Inject constructor(
-//    private val dictionaryService : DictionaryService
-//) {
-//    fun getDictionaryByOpenApi(serviceKey: String, numOfRows: String, pageNo: String) : Flow<ResponseBodys> = flow{
-//        val result =dictionaryService.getDictionaryByOpenApi(serviceKey,numOfRows,pageNo)
-//        emit(result)
-//    }.catch {
-//        Log.e("Get Dictionary By OpenApi Failure", it.message.toString())
-//    }
-//}
