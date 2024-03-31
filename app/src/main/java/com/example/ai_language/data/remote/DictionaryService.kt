ce5dcd9ed983ce17ac4b1f8f0f8f0cd82ab1f245
jiwon <pjw99032986@gmail.com>
2024-03-31 11:36:21 +0900

    refactor : 서비스 코드에 Json 헤더 추가

--- app/src/main/java/com/example/ai_language/data/remote/DictionaryService.kt
+++ app/src/main/java/com/example/ai_language/data/remote/DictionaryService.kt
@@ -3,10 +3,12 @@ package com.example.ai_language.data.remote
 import com.example.ai_language.domain.model.response.ResponseBodys
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Headers
 import retrofit2.http.Query
 
 interface DictionaryService {
     @GET("/openapi/service/rest/meta13/getCTE01701")
+    @Headers("Content-Type: application/json", "Accept: application/json")
     suspend fun getDictionaryByOpenApi(
         @Query("serviceKey") serviceKey: String,
         @Query("numOfRows") numOfRows: String,
