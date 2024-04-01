2d2d162e25133d02064aee4dc5560012369b60dc
jiwon <pjw99032986@gmail.com>
2024-04-02 05:29:32 +0900

    feat: 수어통역센터 OpenApi BaseURL 주소 추가

--- app/src/main/java/com/example/ai_language/Util/Util.kt
+++ app/src/main/java/com/example/ai_language/Util/Util.kt
@@ -3,5 +3,5 @@ package com.example.ai_language.Util
 object Util {
     const val BASE_URL = "http://34.64.212.107:8080/"
     const val BASE_URL2 = "http://api.kcisa.kr"
-    const val BASE_URL3 = "https://openapi.gg.go.kr/Signlangintrprtcenter"
+    const val BASE_URL3 = "https://openapi.gg.go.kr/"
 }
