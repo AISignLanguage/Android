0d962a3cd7d61f39ca27e620f0d9215db7d5229a
jiwon <pjw99032986@gmail.com>
2024-04-02 02:19:03 +0900

    feat: 맵 프레그먼트 추가

---
+++ app/src/main/java/com/example/ai_language/ui/map/MapFragment.kt
@@ -0,0 +1,15 @@
+package com.example.ai_language.ui.map
+
+import com.example.ai_language.R
+import com.example.ai_language.base.BaseFragment
+import com.example.ai_language.databinding.FragmentMapBinding
+
+class MapFragment : BaseFragment<FragmentMapBinding>(R.layout.fragment_map) {
+
+    override fun setLayout() {
+
+    }
+
+
+
+}
