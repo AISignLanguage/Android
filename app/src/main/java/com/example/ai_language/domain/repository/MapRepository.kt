c4af875ca5b7d39a8071ff047f2791d992851cbe
jiwon <pjw99032986@gmail.com>
2024-04-06 21:31:17 +0900

    chore : 미사용 종속성 , 불필요 공백 제거

--- app/src/main/java/com/example/ai_language/domain/repository/MapRepository.kt
+++ app/src/main/java/com/example/ai_language/domain/repository/MapRepository.kt
@@ -1,10 +1,8 @@
 package com.example.ai_language.domain.repository
 
 import com.example.ai_language.domain.model.response.ApiResponse
-import com.example.ai_language.domain.model.response.ResponseBodys
 import kotlinx.coroutines.flow.Flow
 
 interface MapRepository {
     suspend fun getCorporationByOpenApi(KEY: String, Type: String, pIndex: Int, pSize : Int): Flow<ApiResponse>
-
 }
