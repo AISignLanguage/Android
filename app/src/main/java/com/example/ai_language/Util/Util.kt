9d5d9a919b0178ad5eba73fb784773d36d9d5751
jiwon <pjw99032986@gmail.com>
2024-04-09 15:06:19 +0900

    feat : speechflow, youtube api 추가

--- app/src/main/java/com/example/ai_language/Util/Util.kt
+++ app/src/main/java/com/example/ai_language/Util/Util.kt
@@ -6,4 +6,6 @@ object Util {
     const val BASE_URL3 = "https://openapi.gg.go.kr/"
     const val BASE_URL4 = "https://naveropenapi.apigw.ntruss.com/"
     const val BASE_URL5 = "https://apis.openapi.sk.com/"
+    const val BASE_URL6 = "https://api.speechflow.io/"
+    const val BASE_URL7 = "https://www.googleapis.com/youtube/v3/"
 }
