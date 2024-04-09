6287a8d5ed71da3765c4e3d07ae5b75653ff0bcc
jiwon <pjw99032986@gmail.com>
2024-04-09 15:03:26 +0900

    feat : Local input fragment kotlin file 추가

---
+++ app/src/main/java/com/example/ai_language/ui/translation/LocalFileFragment.kt
@@ -0,0 +1,11 @@
+package com.example.ai_language.ui.translation
+
+import com.example.ai_language.R
+import com.example.ai_language.base.BaseFragment
+import com.example.ai_language.databinding.FragmentLocalFileBinding
+
+class LocalFileFragment : BaseFragment<FragmentLocalFileBinding>(R.layout.fragment_local_file) {
+    override fun setLayout() {
+
+    }
+}
