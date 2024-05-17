660482a377374e2846984f4b3dd5c73daa8a3e09
seongmin <arcn1345@gmail.com>
2024-05-17 16:40:36 +0900

    feat: CallListService 불필요 코드 제거

--- app/src/main/java/com/example/ai_language/data/remote/CallListService.kt
+++ app/src/main/java/com/example/ai_language/data/remote/CallListService.kt
@@ -11,12 +11,3 @@ interface CallListService {
         @Body phoneNumberDTO: PhoneNumberDTO
     ): PhoneListDTO
 }
-//interface DictionaryService {
-//    @GET("/openapi/service/rest/meta13/getCTE01701")
-//    @Headers("Content-Type: application/json", "Accept: application/json")
-//    suspend fun getDictionaryByOpenApi(
-//        @Query("serviceKey") serviceKey: String,
-//        @Query("numOfRows") numOfRows: String,
-//        @Query("pageNo") pageNo: String
-//    ): ResponseBodys
-//}
