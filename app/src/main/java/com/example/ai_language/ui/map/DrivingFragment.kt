c05597e741ca44445a46dfdc69679e9614fcaf84
jiwon <pjw99032986@gmail.com>
2024-05-25 20:20:34 +0900

    refactor : recyclerview 조정

--- app/src/main/java/com/example/ai_language/ui/map/DrivingFragment.kt
+++ app/src/main/java/com/example/ai_language/ui/map/DrivingFragment.kt
@@ -56,12 +56,12 @@ class DrivingFragment : BaseFragment<FragmentDrivingBinding> (R.layout.fragment_
     private fun formatTimeDis(time: String) {
         val fTime = time.toInt()
         val timeFormatter = DateTimeFormatter.ofPattern("h:mm a")
-        binding.tvDepartureTime.text = LocalDateTime.now().format(timeFormatter)
+        binding.tvDepartureTime.text = "출발 시간 :"+LocalDateTime.now().format(timeFormatter)
         if (fTime > 60) {
             if (fTime > 3600) {
-                binding.tvArriveTime.text=formatCurrentTimeWithAddedTime((fTime / 3600).toLong(), (fTime / 60).toLong()).replace("오후" , "PM").replace("오전","AM")
+                binding.tvArriveTime.text="도착 시간 :"+formatCurrentTimeWithAddedTime((fTime / 3600).toLong(), (fTime / 60).toLong()).replace("오후" , "PM").replace("오전","AM")
             } else
-                binding.tvArriveTime.text=formatCurrentTimeWithAddedTime(0, (fTime / 60).toLong()).replace("오후" , "PM").replace("오전","AM")
+                binding.tvArriveTime.text="도착 시간 :"+formatCurrentTimeWithAddedTime(0, (fTime / 60).toLong()).replace("오후" , "PM").replace("오전","AM")
         } else {
             "0분"
         }
@@ -96,7 +96,7 @@ class DrivingFragment : BaseFragment<FragmentDrivingBinding> (R.layout.fragment_
                     val fet = response.features
                     if (response.features.isNotEmpty()) {
                         binding.tvAddress.text =
-                            "${mapViewModel.startLoc.value}\n<->${mapViewModel.endLoc.value}"
+                            "${mapViewModel.startLoc.value}\n<->\n${mapViewModel.endLoc.value}"
                         val adapterList = mutableListOf<Feature>()
                         val lineList = mutableListOf<Feature>()
                         val pathOverlay = PathOverlay() // 각 단계별로 새 PathOverlay 생성
